0de48435b109a284eb52fc9551ce654b0c4c9688
gilbertlua <gilbertgmtidn@gmail.com>
2023-07-31 16:53:48 +0700

    day 1 upload

---
+++ day1/Project1/Cat.cs
@@ -0,0 +1,25 @@
+class Cat //Blue Print
+{
+	public bool isSmooth;//Camel Case
+	public string name;
+	public bool isMale;
+	public int age ;
+	public bool isOwner;
+	
+	public void MakeSound()//Pascal Case
+	{
+		Console.WriteLine("Make Sound");
+	}
+	public void Scratch()
+	{
+		Console.WriteLine("Scratch");
+	}
+	public void Sleep()
+	{
+		Console.WriteLine("Sleep");
+	}
+	public void Eat()
+	{
+		Console.WriteLine("Eat");
+	}
+}
---
+++ day1/Project1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+	public static void Main()
+	{
+		Cat anggora = new Cat();//wujud asli blue print
+		anggora.MakeSound();
+		anggora.name = "alex";
+		anggora.age = 12;
+		anggora.isMale =true;
+		anggora.isOwner =true;
+		Console.WriteLine(anggora.name);
+		Console.WriteLine(anggora.age);
+		anggora.Eat();
+		
+	}
+}
