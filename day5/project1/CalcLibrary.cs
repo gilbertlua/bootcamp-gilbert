9aefbcd2e82994247b8711710f2f54a890476f09
gilbertlua <gilbertgmtidn@gmail.com>
2023-08-04 15:14:25 +0700

    day5 commit

---
+++ day5/project1/CalcLibrary.cs
@@ -0,0 +1,20 @@
+namespace CalcLibrary;
+public class Calculator
+{
+	public static int Add(int x, int y)
+	{
+		return x+y;
+	}
+	public static int Multiply(int x, int y)
+	{
+		return x*y;
+	}
+	public static int Devided(int x, int y)
+	{
+		return x/y;
+	}
+	public static int Subtract(int x, int y)
+	{
+		return x-y;
+	}
+}
---
+++ day5/project1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using CalcLibrary;
+class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(Calculator.Subtract(10,2));		
+		Console.WriteLine(Calculator.Add(10,2));
+		Console.WriteLine(Calculator.Multiply(12,2));
+		Console.WriteLine(Calculator.Devided(10,2));
+	}
+}
+
