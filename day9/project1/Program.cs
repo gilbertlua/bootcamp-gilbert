64fd830c62b2f5848e1d52d55b83036387d9c89d
gilbertlua <gilbertgmtidn@gmail.com>
2023-08-14 12:50:39 +0700

    statsh new

--- day9/project1/Program.cs
+++ day9/project1/Program.cs
@@ -6,10 +6,22 @@ class Program{
 	
 	public static void Testing()
 	{
+<<<<<<< Updated upstream
 		
 		FooBar newFooBar = new FooBar();
 		newFooBar.AddModels(1,"foo");
 		// newFooBar.CheckModuls();
+=======
+		int limit = 15;
+		FooBar newFooBar = new FooBar(); 		
+		for(int i=1;i<=limit;i++)
+		{
+			newFooBar.SetNumParameter(i);
+			newFooBar.GenFoobar();
+			string result = newFooBar.GetStrOutput();
+			Console.WriteLine(result);
+		}
+>>>>>>> Stashed changes
 	} 
 }
 
