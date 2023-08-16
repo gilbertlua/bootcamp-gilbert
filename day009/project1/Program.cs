e065b2405ba5686c7952c3fc029b49a629354961
gilbertlua <gilbertgmtidn@gmail.com>
2023-08-16 11:14:39 +0700

    update foobar

--- day009/project1/Program.cs
+++ day009/project1/Program.cs
@@ -2,23 +2,17 @@
 class Program{
 	static void Main(){
 		FooBar newFooBar = new FooBar();
-		newFooBar.AddRules(1,"foo");
-		newFooBar.AddRules(2,"bar");
-		
-		// Console.WriteLine(newFooBar._dict(1));
+		newFooBar.AddRules(3,"foo");
+		newFooBar.AddRules(5,"bar");
+		newFooBar.AddRules(7,"fizz");
+		newFooBar.AddRules(9,"fuzz");
+		newFooBar.AddRules(9,"fazz");
+
+		// Console.WriteLine(newFooBar.RangesCheck(15));
+		Console.WriteLine(newFooBar.RangesCheck(15));
+		// string result = newFooBar.CheckRules();
+		// Console.WriteLine(result);
+
 	}
-	
-	public static void Testing()
-	{
-		int limit = 15;
-		FooBar newFooBar = new FooBar(); 		
-		for(int i=1;i<=limit;i++)
-		{
-			newFooBar.SetNumParameter(i);
-			newFooBar.GenFoobar();
-			string result = newFooBar.GetStrOutput();
-			Console.WriteLine(result);
-		}
-	} 
 }
 
